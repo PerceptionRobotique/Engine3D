51ba35e245a11a3b278ad87f4138715b0fa6d928
Jordan CARACOTTE <29665323+JobiwanD@users.noreply.github.com>
2025-07-16 16:01:36 +0200

    Add openvr port

--- cmake/FindOpenVR.cmake
+++ cmake/FindOpenVR.cmake
@@ -1,24 +1,82 @@
-if(WIN32)
-    set(OPENVR_DIR "OPENVR_DIR-NOTFOUND" CACHE PATH "Directory of OpenVR")
-    if(IS_DIRECTORY ${OPENVR_DIR})
-        list(APPEND OPENVR_INCLUDE_DIRS ${OPENVR_DIR}/headers)
-        set(OPENVR_LIBRARIES ${OPENVR_DIR}/lib/win64/openvr_api.lib)
-    else()
-        message(FATAL_ERROR "OpenVR is missing.")
-    endif()
-elseif(UNIX)
-    set(OPENVR_LIBRARIES openvr_api)
-endif()
+# cmake/FindOpenVR.cmake
+cmake_minimum_required(VERSION 3.22)
 
-list(APPEND HEADERS include/VRheadset.h)
-list(APPEND SOURCES src/VRheadset.cpp)
+function(_OpenVR_find)
+  include(FindPackageHandleStandardArgs)
+  include(SelectLibraryConfigurations)
 
-add_compile_definitions(HAVE_OpenVR)
+  ## Find the include path
+  find_path(OpenVR_INCLUDE_DIR NAMES openvr.h)
 
-### INSTALL ###
+  ## Determine version by scanning header
+  if (OpenVR_INCLUDE_DIR)
+    set(openvr_header "${OpenVR_INCLUDE_DIR}/openvr.h")
 
-if(WIN32)
-    install(FILES "${OPENVR_DIR}/bin/win64/openvr_api.dll" DESTINATION bin/$<CONFIG>/3rdParty)
-    install(DIRECTORY ${OPENVR_INCLUDE_DIRS}/ DESTINATION include/3rdParty/OpenVR)
-    install(FILES "${OPENVR_DIR}/lib/win64/openvr_api.lib" DESTINATION lib/$<CONFIG>/3rdParty/OpenVR)
-endif()
+    set(re_major "^\tstatic const uint32_t k_nSteamVRVersionMajor = ([0-9]+).*;$")
+    set(re_minor "^\tstatic const uint32_t k_nSteamVRVersionMinor = ([0-9]+).*;$")
+    set(re_patch "^\tstatic const uint32_t k_nSteamVRVersionBuild = ([0-9]+).*;$")
+
+    file(STRINGS "${openvr_header}" OpenVR_VERSION_MAJOR REGEX "${re_major}")
+    file(STRINGS "${openvr_header}" OpenVR_VERSION_MINOR REGEX "${re_minor}")
+    file(STRINGS "${openvr_header}" OpenVR_VERSION_PATCH REGEX "${re_patch}")
+
+    string(REGEX REPLACE "${re_major}" "\\1"
+           OpenVR_VERSION_MAJOR "${OpenVR_VERSION_MAJOR}")
+    string(REGEX REPLACE "${re_minor}" "\\1"
+           OpenVR_VERSION_MINOR "${OpenVR_VERSION_MINOR}")
+    string(REGEX REPLACE "${re_patch}" "\\1"
+           OpenVR_VERSION_PATCH "${OpenVR_VERSION_PATCH}")
+
+    if (OpenVR_VERSION_MAJOR AND OpenVR_VERSION_MINOR AND OpenVR_VERSION_PATCH)
+      set(OpenVR_VERSION
+          "${OpenVR_VERSION_MAJOR}.${OpenVR_VERSION_MINOR}.${OpenVR_VERSION_PATCH}")
+    endif ()
+  endif ()
+
+  ## Find the library
+  find_library(OpenVR_LIBRARY_RELEASE NAMES openvr_api)
+
+  # OpenVR uses a highly non-standard additional suffix to mark debug libraries
+  list(TRANSFORM CMAKE_FIND_LIBRARY_SUFFIXES APPEND ".dbg")
+  find_library(OpenVR_LIBRARY_DEBUG NAMES openvr_api)
+
+  select_library_configurations(OpenVR)
+
+  ## Perform all the standard required, version, etc. argument checks.
+  find_package_handle_standard_args(
+    OpenVR
+    REQUIRED_VARS OpenVR_LIBRARY OpenVR_INCLUDE_DIR
+    VERSION_VAR OpenVR_VERSION
+    HANDLE_VERSION_RANGE
+    HANDLE_COMPONENTS
+  )
+
+  ## Create OpenVR::OpenVR imported target.
+  if (OpenVR_FOUND AND NOT OpenVR::OpenVR)
+    add_library(OpenVR::OpenVR UNKNOWN IMPORTED)
+    target_include_directories(OpenVR::OpenVR INTERFACE "${OpenVR_INCLUDE_DIR}")
+    set_target_properties(
+      OpenVR::OpenVR PROPERTIES IMPORTED_LOCATION "${OpenVR_LIBRARY}"
+    )
+
+    foreach (cfg IN ITEMS RELEASE DEBUG)
+      if (OpenVR_LIBRARY_${cfg})
+        set_property(
+          TARGET OpenVR::OpenVR APPEND PROPERTY IMPORTED_CONFIGURATIONS ${cfg}
+        )
+        set_target_properties(
+          OpenVR::OpenVR PROPERTIES
+          IMPORTED_LOCATION_${cfg} "${OpenVR_LIBRARY_${cfg}}"
+        )
+      endif ()
+    endforeach ()
+  endif ()
+
+  ## Export whitelisted variables
+  set(OpenVR_FOUND "${OpenVR_FOUND}" PARENT_SCOPE)
+  set(OpenVR_VERSION "${OpenVR_VERSION}" PARENT_SCOPE)
+
+  add_compile_definitions(HAVE_OpenVR)
+endfunction()
+
+_OpenVR_find()
